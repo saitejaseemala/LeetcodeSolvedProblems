7a0270c9b7f515c2d7e0f614e8d6ccced8faa2d2
Saiteja <saitejaseemala.2424@gmail.com>
2021-09-11 18:08:36 +0530

    Update number-of-steps-to-reduce-a-number-to-zero.java

--- number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,19 +1,13 @@
 class Solution {
+    
     public int numberOfSteps(int num) {
         
-        int count = 0;
-        while ( num > 0 ){
-            
-            if(num%2 == 0){
-                num = num/2;
-                count++;
-            }
-            else {
-                num = num - 1;
-                count++;
-            }
+        if(num != 0){ 
+            if(num%2 == 0) 
+                return 1+numberOfSteps(num/2);
+            else 
+                return 1+numberOfSteps(num-1);     
         }
-        
-        return count;
+        return 0;
     }
 }
