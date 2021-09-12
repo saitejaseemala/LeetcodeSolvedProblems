27ade492e0d9a6d7f6f1021090f045829d37075c
Saiteja <saitejaseemala.2424@gmail.com>
2021-09-12 13:34:47 +0530

    Update add-two-numbers.java

--- add-two-numbers/add-two-numbers.java
+++ add-two-numbers/add-two-numbers.java
@@ -9,33 +9,24 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode temp1 = l1, temp2 = l2;
-        ListNode res = new ListNode(0);
-        ListNode itr = res;
-        
-        int sum = 0;
-        
-        while(temp1 != null || temp2 != null) {
-            sum /= 10;
-            
-            if(temp1 != null) {
-                sum += temp1.val;
-                temp1 = temp1.next;
-            }
-            if(temp2 != null) {
-                sum += temp2.val;
-                temp2 = temp2.next;
-            }
-            
-            itr.next = new ListNode(sum % 10);
-            itr = itr.next;
-        }
-        if(sum / 10 == 1) {
-            itr.next = new ListNode(1);
-        }
-        
-        return res.next;
-    }
     
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummyHead = new ListNode(0);
+    ListNode p = l1, q = l2, curr = dummyHead;
+    int carry = 0;
+    while (p != null || q != null) {
+        int x = (p != null) ? p.val : 0;
+        int y = (q != null) ? q.val : 0;
+        int sum = carry + x + y;
+        carry = sum / 10;
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+        if (p != null) p = p.next;
+        if (q != null) q = q.next;
+    }
+    if (carry > 0) {
+        curr.next = new ListNode(carry);
+    }
+    return dummyHead.next;
+}
 }
