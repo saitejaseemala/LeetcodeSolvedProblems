1b736811a044c3d7b259cf999e1b474ea45c188a
Saiteja <saitejaseemala.2424@gmail.com>
2021-09-12 21:19:14 +0530

    Update linked-list-cycle.java

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -10,20 +10,22 @@
  * }
  */
 public class Solution {
+    
     public boolean hasCycle(ListNode head) {
-        if (head==null) return false;  //check if head is null 
         
-		ListNode slow=head;  //slow pointer
-        ListNode fast=head;  //fast pointer
+	if (head==null) return false;  //check if head is null 
         
-		while(fast.next!=null && fast.next.next!=null)
-		{
-             slow=slow.next;
-             fast=fast.next.next;
-             
-			 if (slow==fast)   //at any time if slow and fast are equal then there is loop
-				 return true;
-         }
+	ListNode slow=head;  //slow pointer
+	ListNode fast=head;  //fast pointer
+
+	while(fast.next!=null && fast.next.next!=null)
+	{
+		slow=slow.next;
+		fast=fast.next.next;
+
+		if (slow==fast)   //at any time if slow and fast are equal then there is loop
+		{return true;}
+	}
         return false;
     }
     
