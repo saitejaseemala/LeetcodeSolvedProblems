9e547d263c68069512485c50e6dca1545e2fc093
Saiteja <saitejaseemala.2424@gmail.com>
2021-09-16 00:50:09 +0530

    Time: 5 ms (5.74%), Space: 38.7 MB (22.92%) - LeetHub

--- check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        
-       String str1 = "";
-       String str2 = ""; 
-       for(int i=0;i<word1.length;i++){
-            str1 = str1+word1[i];
-       } 
-       
-        for(int j=0;j<word2.length;j++){
-            str2 = str2+word2[j];
+        int idx1 = 0, idx2 = 0, arrIdx1 = 0, arrIdx2 = 0;
+        while (arrIdx1 < word1.length && arrIdx2 < word2.length) {
+            if (word1[arrIdx1].charAt(idx1) != word2[arrIdx2].charAt(idx2)) return false;
+            idx1 = (++idx1) % word1[arrIdx1].length();
+            idx2 = (++idx2) % word2[arrIdx2].length();
+            if (idx1 == 0) arrIdx1++;
+            if (idx2 == 0) arrIdx2++;
         }
-        
-        if(str1.equals(str2)) return true;
-        else
-            return false;
+        return arrIdx1 == word1.length && arrIdx2 == word2.length;
     }
 }
