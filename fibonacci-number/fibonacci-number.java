fe6890d4c8176a5886ee5d0230406e57bf800545
Saiteja <saitejaseemala.2424@gmail.com>
2021-09-19 00:30:31 +0530

    Time: 0 ms (100.00%), Space: 35.7 MB (76.25%) - LeetHub

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,11 +1,6 @@
 class Solution {
-    public int fib(int n) {
-        if(n==1){
-            return 1;
-        }
-        if(n==0){
-            return 0;
-        }
-        return fib(n-1)+fib(n-2);
+    public int fib(int N) {
+        Double gRatio = 1.618034;
+        return (int)((Math.pow(gRatio, N) - Math.pow(1-gRatio, N))/Math.sqrt(5));
     }
 }
